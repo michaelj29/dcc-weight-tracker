791ffe56562bd7f2967545c24fe193ba83856451
Michael J <johnson_michael1329@yahoo.com>
2022-04-19 16:06:08 -0500

    Added AddEntryForm Component.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [entries, setEntries] = useState([{weight: 175, date:'11-11-2022'}])
 
+  function addNewEntry(entry){
+    let tempEntries = [...entries, entry]
+    setEntries(tempEntries)
+
+  }
+
 return (
-<DisplayEntries userEntries={entries}/>
+  <div>
+    <DisplayEntries userEntries={entries}/>
+    <AddEntryForm addNewEntryProperty={addNewEntry} />
+  </div>
 );
 
 }
